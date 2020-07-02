585f9066d6b507770496c0d64fc116cdc062a270
Nino-44 <67273003+Nino-44@users.noreply.github.com>
2020-07-02 16:52:51 +0200

    Add files via upload

---
+++ commandes.php
@@ -0,0 +1,10 @@
+<?php
+
+$historique_commandes = array('5.49', '9.99', '5.49', '15.99', '25');
+
+
+    foreach ($historique_commandes as $historique_commande) {
+        echo "Une commande de $historique_commande euros a été reçus." ,'<br>';
+    }
+
+?>
